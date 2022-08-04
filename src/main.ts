b6b7644f3ee300d67a3f4e502bc29652f4b3c9f6
kuba1pie <kuba1pie@gmail.com>
2022-08-05 00:42:20 +0200

    plugin: pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.sass'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.sass";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+import routes from "~pages";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,11 @@ export default defineConfig({
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: "src/components.d.ts",
+    }),
+    Pages({
+      dirs: [
+        { dir: "src/pages", baseRoute: "" },
+      ],
     }),
   ],
 });
