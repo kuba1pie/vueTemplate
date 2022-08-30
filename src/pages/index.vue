e72a7b07306eaf7512b27d49016d00364c6999ff
kuba1pie <kuba1pie@gmail.com>
2022-08-30 18:58:40 +0200

    feat: prettier

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,10 @@
 <template>
   <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      >
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="../assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      >
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -31,6 +17,6 @@
   will-change: filter
   &:hover
     filter: drop-shadow(0 0 2em #646cffaa)
-.logo.vue:hover 
+.logo.vue:hover
   filter: drop-shadow(0 0 2em #42b883aa)
 </style>
